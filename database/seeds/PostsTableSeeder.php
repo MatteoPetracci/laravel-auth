9f3ddeb553fdc88cdceef45983aece05fc31e1a3
Matteo <matteop18@gmail.com>
2020-03-30 16:53:21 +0200

    creo seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+Use App\User;
+use App\Post;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +13,18 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 20; $i++) { 
+            // Instanzio un nuovo post
+            $newPost = new Post;
+            // Prendo l'id di questo utente
+            $newPost->user_id = 1;
+            $newPost->image = "https://picsum.photos/id/" . rand(1,100) . '/300/200';
+            $newPost->title = $faker->text(10);
+            $newPost->body = $faker->paragraph();
+            $newPost->slug = Str::slug($newPost->title, '/');
+            $newPost->save();
+        }
     }
 }

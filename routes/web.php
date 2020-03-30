efa4272d3edda85416d850e87f167a2ce56de71d
Matteo <matteop18@gmail.com>
2020-03-30 17:09:31 +0200

    prova stampa solo quando sono in admin

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
     // Inserisco le varie rotte in admin
     Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+
 });

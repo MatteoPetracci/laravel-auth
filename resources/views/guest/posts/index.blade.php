dacec828d0cc9d98d50ca783792b5abd4bb8e1e1
Matteo <matteop18@gmail.com>
2020-03-31 16:26:40 +0200

    stampo i posts nella index guest

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -1,1 +1,15 @@
-@dd($posts)
+{{-- @dd($posts) --}}
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                @foreach ($posts as $post)
+                    <h3>{{$post->title}}</h3>
+                    <p>{{$post->user->name}}</p>
+                    <p>{{$post->body}}</p>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
